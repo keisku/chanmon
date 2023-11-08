858951a7c4f75a8957b4cecf3c597efe7f51a1a2
keisku <keisuke.umegaki.630@gmail.com>
2023-11-08 13:04:21 +0000

    refactor addr2line/Init

--- addr2line/addr2line.go
+++ addr2line/addr2line.go
@@ -38,81 +38,80 @@ func (syms *symbols) pcToLine(addr uint64) string {
 	}
 }
 
+var initErr error
+
 // Init loads the debug info from the specified binary file and parsing its symbol and line number information.
 // This function is intended to be called once, with future calls being no-ops.
 func Init(binPath string) error {
-	var initErr error
 	once.Do(func() {
-		f, err := elf.Open(binPath)
-		if err != nil {
-			initErr = err
-			return
-		}
-		defer f.Close()
-		gopclntab := f.Section(".gopclntab")
-		if gopclntab != nil {
-			lineTableData, err := gopclntab.Data()
-			if err != nil {
-				initErr = fmt.Errorf("failed to read .gopclntab section: %w", err)
-				return
-			}
-			lineTable := gosym.NewLineTable(lineTableData, f.Section(".text").Addr)
-			s, err := gosym.NewTable(nil, lineTable)
-			if err != nil {
-				initErr = fmt.Errorf("failed to parse symbols: %w", err)
-				return
-			}
-			syms = symbols{
-				s: s,
-			}
-			// If symbols are successfully loaded from `.gopclntab`, skip loading DWARF.
-			// `.gopclntab` has enough information.
-			slog.Debug("load symbols from .gopclntab")
-			return
-		}
-
-		// Fallback to DWARF when loading `.gopclntab` fails.
-		// Reference code: https://github.com/golang/go/blob/go1.21.3/src/debug/dwarf/line_test.go#L181-L255
-		d, err := f.DWARF()
-		if err != nil {
-			initErr = fmt.Errorf("failed to read DWARF: %w", err)
-			return
-		}
-		reader := d.Reader()
-		for {
-			cu, err := reader.Next()
-			if err != nil {
-				initErr = err
-				return
-			}
-			if cu == nil {
-				break
-			}
-			lr, err := d.LineReader(cu)
-			if err != nil {
-				initErr = err
-				return
-			}
-			if lr == nil {
-				continue
-			}
-			var line dwarf.LineEntry
-			for {
-				if err := lr.Next(&line); err != nil {
-					if err == io.EOF {
-						break
-					}
-					initErr = err
-					return
-				}
-				if line.File != nil {
-					lineEntries.Store(line.Address, fmt.Sprintf("%s:%d", line.File.Name, line.Line))
-				}
-			}
-		}
-		slog.Debug("load symbols from DWARF")
+		initErr = initialize(binPath)
 	})
 	return initErr
+}
+
+func initialize(binPath string) error {
+	f, err := elf.Open(binPath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	gopclntab := f.Section(".gopclntab")
+	if gopclntab != nil {
+		lineTableData, err := gopclntab.Data()
+		if err != nil {
+			return fmt.Errorf("failed to read .gopclntab section: %w", err)
+		}
+		lineTable := gosym.NewLineTable(lineTableData, f.Section(".text").Addr)
+		s, err := gosym.NewTable(nil, lineTable)
+		if err != nil {
+			return fmt.Errorf("failed to parse symbols: %w", err)
+		}
+		syms = symbols{
+			s: s,
+		}
+		// If symbols are successfully loaded from `.gopclntab`, skip loading DWARF.
+		// `.gopclntab` has enough information.
+		slog.Debug("load symbols from .gopclntab")
+		return nil
+	}
+
+	// Fallback to DWARF when loading `.gopclntab` fails.
+	// Reference code: https://github.com/golang/go/blob/go1.21.3/src/debug/dwarf/line_test.go#L181-L255
+	d, err := f.DWARF()
+	if err != nil {
+		return fmt.Errorf("failed to read DWARF: %w", err)
+	}
+	reader := d.Reader()
+	for {
+		cu, err := reader.Next()
+		if err != nil {
+			return err
+		}
+		if cu == nil {
+			break
+		}
+		lr, err := d.LineReader(cu)
+		if err != nil {
+			return err
+		}
+		if lr == nil {
+			continue
+		}
+		var line dwarf.LineEntry
+		for {
+			if err := lr.Next(&line); err != nil {
+				if err == io.EOF {
+					break
+				}
+				return err
+			}
+			if line.File != nil {
+				lineEntries.Store(line.Address, fmt.Sprintf("%s:%d", line.File.Name, line.Line))
+			}
+		}
+	}
+	slog.Debug("load symbols from DWARF")
+	return nil
 }
 
 // Do converts a program counter address to a file name and line number,
